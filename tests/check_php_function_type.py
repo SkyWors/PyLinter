5d6af0d51b157529149e0139fe2d776a29385b4a
SkyWors <skyworslegrand@gmail.com>
2025-03-02 23:27:07 +0100

    Update program

--- tests/check_php_function_type.py
+++ tests/check_php_function_type.py
@@ -2,5 +2,5 @@ from messages.error import errorMessage
 
 def check_php_function_type(filePath, i, line):
 	if "function" in line:
-		if "):" in line:
+		if ") :" in line:
 			errorMessage(filePath, i, "Malformed function type.")
